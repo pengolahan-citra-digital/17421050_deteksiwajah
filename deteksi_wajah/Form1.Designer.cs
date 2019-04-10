941567fd64ca1824e3152f5290f2126727ab721a
davidandika15 <davidandika02@gmail.com>
2019-04-10 11:39:37 +0700

    deteksi wajah

---
+++ deteksi_wajah/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace deteksi_wajah
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.btn_kamera = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(3, 22);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(267, 164);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // btn_kamera
+            // 
+            this.btn_kamera.Location = new System.Drawing.Point(138, 202);
+            this.btn_kamera.Name = "btn_kamera";
+            this.btn_kamera.Size = new System.Drawing.Size(115, 23);
+            this.btn_kamera.TabIndex = 3;
+            this.btn_kamera.Text = "bukakamera";
+            this.btn_kamera.UseVisualStyleBackColor = true;
+            this.btn_kamera.Click += new System.EventHandler(this.btn_kamera_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btn_kamera);
+            this.Controls.Add(this.imageBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button btn_kamera;
+
+    }
+}
+
---
+++ deteksi_wajah/Form1.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+
+
+namespace deteksi_wajah
+{
+    public partial class Form1 : Form
+    {
+        Capture capture;
+        bool captureprogres;
+        HaarCascade Haar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_kamera_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+                }
+                catch (NoNullAllowedException exc)
+                {
+                    MessageBox.Show("camera tidak aktif");
+                }
+            }
+            if (capture != null)
+            {
+                if (captureprogres)
+                {
+                    Application.Idle -= prosesframe;
+                }
+                else
+                {
+                    Application.Idle += prosesframe;
+                }
+                captureprogres = !captureprogres;
+            }
+
+        }
+        private void prosesframe(object sender, EventArgs arg)
+        {
+            Image<Bgr, Byte> ImageFrame = capture.QueryFrame();
+            if (ImageFrame != null)
+            {
+                Image<Gray, Byte> grayframe = ImageFrame.Convert<Gray, Byte>();
+                var face = grayframe.DetectHaarCascade(Haar, 1.4, 4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20))[0];
+                foreach (var faces in face)
+                {
+                    ImageFrame.Draw(faces.rect, new Bgr(Color.Red), 3);
+                }
+
+            }
+            imageBox2.Image = ImageFrame;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Haar = new HaarCascade("haarcascade_frontalface_default.xml");
+        }
+    }
+}
